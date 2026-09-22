d065db3f265fd5c01a99c24f5af43124c6c06906
Alan-JC <cruz.arenas.alanjair@hotmail.com>
2026-09-22 17:10:29 -0600

    La calculadora actualizada que pide nombre, Ro :)

---
+++ calculadora-js/calculadora-js.js
@@ -0,0 +1,8 @@
+let nombre = prompt ("Como te llamas?"); 
+console.log("Hola "+ nombre + ", estos son tus resultados"); 
+let numero = Number(prompt("Escribe el numero"));
+let numero2 = Number(prompt("Escribe otro numero"));
+console.log("Suma", numero+numero2);
+console.log("Resta", numero-numero2);
+console.log("Multiplicación", numero*numero2);
+console.log("División", numero/numero2)
