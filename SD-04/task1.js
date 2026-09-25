e6ab3ec3a2340078c9af9862b1b2ae22dc19960d
Alan-JC <cruz.arenas.alanjair@hotmail.com>
2026-09-25 17:18:22 -0600

    Lab hecho

---
+++ SD-04/task1.js
@@ -0,0 +1,14 @@
+const arr = []
+
+// Type your code below this line!
+
+for (let i = 1; i <= 20; i++) {
+    arr.push(i);
+}
+
+console.log (arr);
+
+
+// Type your code above this line!
+
+arr.forEach(element => console.log(element))
---
+++ SD-04/task2.js
@@ -0,0 +1,15 @@
+const arr = [16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+
+// Type your code below this line!
+
+arr.reverse();
+
+console.log(arr);
+
+arr.forEach(element => console.log(element));
+
+
+
+// Type your code above this line!
+
+arr.forEach(element => console.log(element))
